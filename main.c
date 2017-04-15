917520740f2ecc405749967118088be2c9d41903
tyleryy <w97245873@live.cn>
2017-04-15 22:04:12 +0800

    add some function
    receive and parse data successfully

--- main.c
+++ main.c
@@ -1,13 +1,47 @@
 #include "usart.h"
-#include"playvoice.h"
-#include"init.h"
+#include "playvoice.h"
+#include "init.h"
+#include "datalink/datalink.h"
+#include "stdio.h"
+
+#define MAINDEBUG
+
+#ifdef MAINDEBUG
+#define main_debug(fmt, ...)  printf(fmt, ##__VA_ARGS__)
+#else
+#define main_debug(fmt, ...)
+#endif
 
 int main(void)
 {
 	initialise_board();
-
+	s16 signal1 = 0, signal2 = 0, attention1 = 0, attention2 = 0;
+	u16 i=0,j=0;
+	main_debug("start");
 	while (1)
 	{
+//		signal2=-2;
+//		signal2 = get_data(SIGNAL2);
+//		if (signal2 > 0)
+//		{
+//			main_debug("signal-------%d----",signal2);
+//			//play_voice(POOR_SINGAL);
+//		}
+//		else if (signal2 = 0)
+//		{
+//			attention2=get_data(ATTENTION2);
+//			main_debug("attention:<%d>",attention2);
+//
+//		}
+		main_debug("<%d>",get_data(SIGNAL2));
+		for(i=0;i<0xffff;i++)
+		{
+			for(j=0;j<0xff;j++)
+			{
+				;
+			}
+		}
+
 
 	}
 }
